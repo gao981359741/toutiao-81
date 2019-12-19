168af2a717e9738840d2e4c1f1ccf534650015b3
gao <981359741@qq.com>
2019-12-19 16:42:10 +0800

    主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>我是主页</div>
+<!-- 因为用了elementUI，所以用他的标签 -->
+<!-- 放置一个最外面的容器 -->
+<el-container>
+  <!-- 先左右布局  再把右边做上下布局-->
+<el-aside>左侧</el-aside>
+<el-container>
+    <el-header>
+        头部
+      </el-header>
+        <!-- 主要区域 -->
+      <el-main>
+        主要区域
+      </el-main>
+</el-container>
+</el-container>
 </template>
 
 <script>
