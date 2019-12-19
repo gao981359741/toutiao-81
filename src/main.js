91ad071c100191cca0fd0645fb9de2545c3bc853
gao <981359741@qq.com>
2019-12-19 16:26:40 +0800

    登录模块-登录成功及失败处理

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'// 引入包
+import axios from 'axios'// 引入axios
 import 'element-ui/lib/theme-chalk/index.css' // 引入ElementUI样式
 import './styles/index.less'// 引入初始化样式
+Vue.prototype.$axios = axios// 赋值给全局对象
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'// 赋值黑马头条的默认地址
 Vue.config.productionTip = false
-Vue.use(ElementUI)// 全局注册
+Vue.use(ElementUI)// 全局注册ElementUI
 new Vue({
   router,
   render: h => h(App)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,24 +6,25 @@
       <div class="title">
         <img src="../../assets/img/logo_index.png" alt />
       </div>
-      <!-- 下面的登录表单 -->
-      <el-form style="margin-top:20px">
+      <!-- 下面的登录表单  给el-from绑定model属性   rules属性绑定验证规则对象-->
+      <el-form ref="myForm" style="margin-top:20px"  :model="loginForm" :rules='loginRules'>
         <!-- 表单域  er-from-item-->
-        <el-form-item>
+        <el-form-item prop="mobile">
           <!-- 放置具体的组件  登录手机号-->
-          <el-input placeholder="请输入手机号"></el-input>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
         <!-- 验证码 -->
-        <el-form-item>
-          <el-input style="width:63%" placeholder="请输入验证码"></el-input>
+        <el-form-item prop="code">
+          <el-input style="width:63%" placeholder="请输入验证码" v-model="loginForm.code"></el-input>
           <el-button plain style="float:right">发送验证码</el-button>
         </el-form-item>
         <el-form-item prop="check">
-          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+          <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <!-- 登录 -->
-         <el-form-item>
-             <el-button style="width:100%" type="primary">登录</el-button>
+         <el-form-item >
+             <!-- 注册点击事件 -->
+             <el-button @click="submiteLogin" style="width:100%" type="primary">登录</el-button>
          </el-form-item>
       </el-form>
     </el-card>
@@ -31,7 +32,75 @@
 </template>
 
 <script>
-export default {}
+export default {
+  // 第一步  在data中定义表单数据
+  data () {
+    return {
+      // 定义表单数据对象
+
+      loginForm: {
+        // 手机号
+        mobile: '',
+        // 验证码
+        code: '',
+        // 是否勾选
+        check: false
+      },
+      loginRules: {
+        // 验证规则  key(字段名)：value(数组)
+        // required：true--必须写
+        // pattern正则表达式
+        mobile: [{ required: true, message: '请输入手机号' }, { pattern: /^1[3456789]\d{9}$/, message: '手机号格式不正确'
+        }],
+        code: [{ required: true, message: '请输入验证码' }, { pattern: /^\d{6}$/,
+          message: '验证码格式不正确' }],
+        // 自定义函数
+        check: [{ validator: function (rule, value, callback) {
+          // rule 当前的规则    可以忽略
+          // value指我们要效验的字段的值
+          if (value) {
+            // 效验通过  执行callback
+            callback()
+          } else {
+            // 认为不通过  并提示信息  固定写法
+            callback(new Error('请同意协议'))
+          }
+        } }]
+
+      }
+    }
+  },
+  methods: {
+    submiteLogin () {
+      // el-form实例
+      this.$refs.myForm.validate((isOK) => {
+        // 认为前端效验登录表单成功  发送去登录请求地址
+        if (isOK) {
+          console.log('成功')
+
+          this.$axios({
+            url: '/authorizations', // 请求地址
+            method: 'post', // 请求类型
+            data: this.loginForm
+            // 成功后进入then
+          }).then(result => {
+            // localStorage本地缓存  特点：关闭浏览器数据不会消失，除非手动删除数据
+            window.localStorage.setItem('user-token', result.data.data.token)// 获取前端缓存令牌
+            this.$router.push('/home')// 跳转到主页
+            // 失败后进入catch
+          }).catch(error => {
+            console.log(error)
+            // message是elementUI的方法
+            this.$message({
+              message: '您的手机号或验证码不正确',
+              type: 'warning'
+            })
+          })
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style lang='less' scoped>
