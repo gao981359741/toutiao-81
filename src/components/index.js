801eca32c4042704819a9c87d6c8cecbb95dca9e
gao <981359741@qq.com>
2019-12-26 17:09:17 +0800

    发布文章-注册富文本编辑器并使用

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,10 @@
 import layoutHeader from './home/layout-header'
 import layoutAside from './home/layout-aside'
 import BreadCrumb from './common/bread-crumb'
+import { quillEditor } from 'vue-quill-editor' // 引入了quill组件
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 // 导出
 export default {
   // 里面有一个固定属性install
@@ -10,5 +14,6 @@ export default {
     Vue.component('layout-header', layoutHeader)
     Vue.component('layout-aside', layoutAside)
     Vue.component('bread-crumb', BreadCrumb)// 面包屑组件
+    Vue.component('quill-editor', quillEditor) // 全局注册富文本编辑器
   }
 }
